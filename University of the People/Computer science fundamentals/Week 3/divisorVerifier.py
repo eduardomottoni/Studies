79f08f5233119c1728b44960c1baba6891f1b4c2
eduardomottoni <56644658+eduardomottoni@users.noreply.github.com>
2021-11-22 16:07:14 -0300

    Ortography review

--- University of the People/Computer science fundamentals/Week 3/divisorVerifier.py
+++ University of the People/Computer science fundamentals/Week 3/divisorVerifier.py
@@ -1,6 +1,7 @@
 
-# my function will reveice a number and tells if its number is divisible by all number until the mid of the range of numbers between 2 and the number of choice
-# update 1.2, now the program tells if the number is prime or not, awasome!
+# my function will receive a number and tell if its number is divisible by all numbers until the mid of the range of numbers between 2 and the number of choices
+
+# update 1.2, now the program tells if the number is prime or not, awesome!
 
 def division(number,amount_divisors):
     i=1
@@ -21,13 +22,13 @@ def main():
         print('Type a number the number to be divided')
         number = int(input())
         amount_divisors = int(number/2)
-        print('press enter to verify the division of {} until {}'.format(number,amount_divisors))
+        print('Press enter to verify the division of {} until {}'.format(number,amount_divisors))
         input()
         isPrime = division(number,amount_divisors)
         if isPrime == True:
             print('The number {} is prime!'.format(number))
         else:
-            print('The number {} is not prime. :/'.format(number))
+            print('The number {} is not prime. :/'.format(number)) #all we love prime numbers, right?
         print()
 
 main()
